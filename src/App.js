8ddf4280dff3a68b492def15314ebecddf3f596f
New User <newuser@News-MBP.charter.com>
2020-11-22 13:14:09 -0800

    feat(get repos  webapp): complete

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './app.scss';
 import { Content } from 'carbon-components-react/lib/components/UIShell';
 import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
-import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
+import SearchPage from './content/SearchPage';
 
 class App extends Component {
   render() {
@@ -13,7 +13,7 @@ class App extends Component {
         <TutorialHeader />
         <Content>
           <Switch>
-            <Route exact path="/" component={LandingPage} />
+            <Route exact path="/" component={SearchPage} />
             <Route path="/repos" component={RepoPage} />
           </Switch>
         </Content>
